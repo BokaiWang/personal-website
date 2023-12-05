911ff8b2e1508d1c2fcd417bce2237638512501c
BokaiWang <bk.wang1989@gmail.com>
2023-12-04 22:09:39 -0500

    Build a navbar; Install classnames and react-icons; Create some basic pages;

---
+++ app/_components/Navbar.tsx
@@ -0,0 +1,41 @@
+"use client";
+import Link from "next/link";
+import { FaCat } from "react-icons/fa";
+import React from "react";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
+
+const Navbar = () => {
+  const pathname = usePathname();
+  const navLinks: { title: string; href: string }[] = [
+    { title: "About", href: "/about" },
+    { title: "Projects", href: "/projects" },
+    { title: "Blog", href: "/blog" },
+    { title: "Contact", href: "/contact" },
+  ];
+  return (
+    <nav className="flex justify-between p-3 text-2xl border-b-stone-500 border-b">
+      <Link href={"/"}>
+        <FaCat />
+      </Link>
+      <ul className="flex gap-5">
+        {navLinks.map((link) => (
+          <li key={link.title}>
+            <Link
+              className={classNames({
+                "duration-300": true,
+                "hover:text-orange-700": true,
+                "text-orange-700": pathname === link.href,
+              })}
+              href={link.href}
+            >
+              {link.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Navbar from "./_components/Navbar";
+
 export default function Home() {
   return <h1>Hello World</h1>;
 }
