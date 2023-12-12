fc091706faa33a4e737d7d97a16605eb2c0b0a38
BokaiWang <bk.wang1989@gmail.com>
2023-12-11 20:36:37 -0500

    Extract home section

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
-export default function Home() {
+import About from "./sections/About";
+import Contact from "./sections/Contact";
+import Home from "./sections/Home";
+import Projects from "./sections/Projects";
+
+export default function Page() {
   return (
     <>
-      <h1 className="text-3xl">Welcome!</h1>
-      <p>
-        I'm Bo-Kai Wang, or simply Kai. As a frontend developer, I specialize in
-        harnessing the power of React.js to create dynamic and engaging user
-        interfaces. My proficiency extends to HTML and CSS, ensuring that every
-        project I undertake is both visually stunning and functionally robust. I
-        am well-versed in state management with Redux.js, allowing me to build
-        scalable and efficient applications. Additionally, my expertise includes
-        seamlessly integrating with RESTful APIs to bring data to life on the
-        web. Let's collaborate to transform ideas into interactive digital
-        experiences!
-      </p>
+      <Home />
+      <About />
+      <Projects />
+      <Contact />
     </>
   );
 }
---
+++ app/sections/Home.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <section id="home" className="ml-auto mr-auto w-full sm:w-3/5">
+      <h1 className="text-3xl">Welcome!</h1>
+      <article>
+        <p>
+          I'm Bo-Kai Wang, or simply Kai. As a frontend developer, I specialize
+          in harnessing the power of React.js to create dynamic and engaging
+          user interfaces. My proficiency extends to HTML and CSS, ensuring that
+          every project I undertake is both visually stunning and functionally
+          robust. I am well-versed in state management with Redux.js, allowing
+          me to build scalable and efficient applications. Additionally, my
+          expertise includes seamlessly integrating with RESTful APIs to bring
+          data to life on the web. Let's collaborate to transform ideas into
+          interactive digital experiences!
+        </p>
+      </article>
+    </section>
+  );
+};
+
+export default Home;
