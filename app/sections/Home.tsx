79c3f85a9962f47a2990f33ebbf257739e9efd99
BokaiWang <bk.wang1989@gmail.com>
2023-12-12 19:59:28 -0500

    Create the home page

--- app/sections/Home.tsx
+++ app/sections/Home.tsx
@@ -3,18 +3,14 @@ import React from "react";
 const Home = () => {
   return (
     <section id="home" className="ml-auto mr-auto w-full sm:w-3/5">
-      <h1 className="text-3xl">Welcome!</h1>
+      <h1 className="text-6xl mt-6 mb-12">Welcome to my playground!</h1>
       <article>
-        <p>
-          I'm Bo-Kai Wang, or simply Kai. As a frontend developer, I specialize
-          in harnessing the power of React.js to create dynamic and engaging
-          user interfaces. My proficiency extends to HTML and CSS, ensuring that
-          every project I undertake is both visually stunning and functionally
-          robust. I am well-versed in state management with Redux.js, allowing
-          me to build scalable and efficient applications. Additionally, my
-          expertise includes seamlessly integrating with RESTful APIs to bring
-          data to life on the web. Let's collaborate to transform ideas into
-          interactive digital experiences!
+        <p className="text-3xl leading-loose">
+          Hello, fellow coders and curious minds! I'm{" "}
+          <span className="font-bold italic">Bo-Kai Wang</span>, a{" "}
+          <span className="font-bold italic">frontend developer</span> fueled by
+          a passion for turning lines of code into captivating user experiences
+          and specializing in React. I enjoy building websites and web apps.
         </p>
       </article>
     </section>
